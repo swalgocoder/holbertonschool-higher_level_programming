445892342c8f010f0646f5b55a2eb8742ed6bd7a
gary gao <gary.d.gao@gmail.com>
2017-01-03 17:40:33 -0800

    a

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(1, 10):
+        if (i < j and not (i == 8 and j == 9)):
+            print('{:d}{:d}, '.format(i, j), end="")
+        if i == 8 and j == 9:
+            print('{:d}'.format(89))
