69e699c65dae080b8ffeaed2af91ffeb23d76165
gary gao <gary.d.gao@gmail.com>
2017-01-07 20:59:19 -0800

    a

---
+++ 0x02-python-import_modules/101-easy_print.py.v1
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+if __name__ == "__main__":
+    import sys
+    str = '#pythoniscool'
+    sys.stdout.write(str + '\n')
--- 0x02-python-import_modules/my_print.py
+++ 0x02-python-import_modules/my_print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python
-if __name__ == "__main__":
-    import sys
-    str = '#pythoniscool'
-    sys.stdout.write(str + '\n')
+print("#pythoniscool")
