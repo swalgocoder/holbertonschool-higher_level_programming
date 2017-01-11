ea9582e4d4e3b658ec2659b66c4d82e3c8d5d82b
gary gao <gary.d.gao@gmail.com>
2017-01-10 23:13:50 -0800

    a

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = str()
+    for element in my_string:
+        if (element != 'c') and (element != 'C'):
+            new_str += element
+    return (new_str)
+            
+    
