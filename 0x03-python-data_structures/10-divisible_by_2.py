5fd97de222dbba919a70542733e1706cb338f3df
gary gao <gary.d.gao@gmail.com>
2017-01-10 23:59:38 -0800

    a

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    my_even = []
+def divisible_by_2(list=[]):
+    my_list = list.copy()
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            my_even.append(True)
+        if list[i] % 2 == 0:
+            my_list[i] = True
         else:
-            my_even.append(False)
-    return (even)
+            my_list[i] = False
+    return my_list
