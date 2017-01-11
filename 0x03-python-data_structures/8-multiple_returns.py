1ea2d6ca6ab8b8627c0bef67ae8c0113f3a49815
gary gao <gary.d.gao@gmail.com>
2017-01-10 23:52:28 -0800

    a

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 def multiple_returns(sentence):
-    if len(sentence) <= 0:
-        return (None)
-    else:
+    if len(sentence) > 0:
         return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), None)
+
+
