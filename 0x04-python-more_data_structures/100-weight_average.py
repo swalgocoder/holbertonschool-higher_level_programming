fb3e662a241309165ed2d08bac6e18ec1b958982
gary gao <gary.d.gao@gmail.com>
2017-01-12 23:52:03 -0800

    a

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,12 @@
 def weight_average(my_list=[]):
     if my_list is None or my_list == []:
         return 0
-    numerator = sum([v * w for v, w in my_list])
-    denominator = sum([w for v, w in my_list])
-    if (denominator != 0):
-        return (float(numerator) / float(denominator))
-    else:
-        return 0
+    numerator = 0
+    denominator = 0
+
+    for (v, w) in my_list:
+        numerator += (v * w)
+        denominator += w
+        return numerator / denominator
+
     
