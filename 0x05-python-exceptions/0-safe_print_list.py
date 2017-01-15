ff8bdd8b9f9bf1a3f33c93c9b4f9b34bc7b562f9
gary gao <gary.d.gao@gmail.com>
2017-01-15 08:59:04 -0800

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     nb_print = 0
-    try:
-        while nb_print < x:
+    for nb_print in range(x):
+        try:
             print('{}'.format(my_list[nb_print]), end = "")
             nb_print += 1
-    except IndexError:
-        print("")
-        return (nb_print)
+        except IndexError:
+            print("")
+            return (nb_print)
     print("")
     return (nb_print)
