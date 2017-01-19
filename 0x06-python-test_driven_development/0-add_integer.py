2244682d53222b15a812dd516a5c547ed9c1d778
gary gao <gary.d.gao@gmail.com>
2017-01-19 12:58:11 -0800

    a

---
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+testing adding two arguments,raising errors if their data types are not floats or ints.
+"""
+
+
+def add_integer(a, b):
+    """
+    Raise TypeError if a or b fails boolean test:
+    Raise TypeError for every case that is not either an integer or a float.
+    Float gets converted to an int.
+    """
+
+    if ((isinstance(a, (int, float))) and isinstance(b, (int, float))):
+        a = int(a)
+        b = int(b)
+        return a + b
+    if (isinstance(a, (int, float)) is False):
+        raise TypeError("a must be an integer")
+    if (isinstance(b, (int, float)) is False):
+        raise TypeError("b must be an integer")
