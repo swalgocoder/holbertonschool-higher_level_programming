0da72bdb2c80cc37a64a59656dccf63995b68a57
gary gao <gary.d.gao@gmail.com>
2017-01-23 20:49:37 -0800

    a

---
+++ 0x07-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+The Square Class creates an empty Square object for now
+The pass statement does noting particular, placeholder;
+ohterwise Indentation Error may occur
+"""
+
+
+class Square:
+    pass
