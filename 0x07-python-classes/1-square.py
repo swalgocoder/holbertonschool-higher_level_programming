ef7378f1a90941d888c62cba08c18be6fb1d3419
gary gao <gary.d.gao@gmail.com>
2017-01-23 21:00:49 -0800

    a

---
+++ 0x07-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+The Square Class creates a Square object with initialized "size"
+which is private attribute
+"""
+
+
+class Square:
+    def __init__(self,size):
+        self.__size = size
