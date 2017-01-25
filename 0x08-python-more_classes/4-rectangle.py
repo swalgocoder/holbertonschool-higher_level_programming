cc7a818d3cdce0dab76a3a838f542778dd837604
gary gao <gary.d.gao@gmail.com>
2017-01-24 20:36:03 -0800

    a

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+    @property
+    def width(self):
+            return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if int(value) < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+        
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if int(value) < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return ((self.__height + self.__width) * 2)
+
+    def __str__(self):
+        """
+        string representation
+        """
+        string = ""
+        if self.__height == 0 or self.__width == 0:
+            return string
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += "#"
+            string += "\n"
+        string = string[:-1]
+        return string
+
+    def __repr__(self):
+        width = str(self.__width)
+        height = str(self.__height)
+        return ("Rectangle(" +width+ ", " +height+ ")")
