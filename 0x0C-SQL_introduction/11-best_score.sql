bdedf37d66e90fe2afa4f64765a2c4355ff02319
gary gao <gary.d.gao@gmail.com>
2017-02-22 12:35:02 -0800

    a

---
+++ 0x0C-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- limit 2
+-- vs where statement
+
+select score, name from second_table order  by score desc limit 2;
---
+++ 0x0C-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- update by set statement
+-- where clause
+
+update second_table set score=10 where name="Bob";
+
