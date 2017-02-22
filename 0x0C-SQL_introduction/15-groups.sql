183bcd7bc7b6bdb7ac35768c8e64ca01903d8f6c
gary gao <gary.d.gao@gmail.com>
2017-02-22 12:59:30 -0800

    a

---
+++ 0x0C-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- select
+-- count(columm name) as
+
+select score, count(score) as name from second_table group by score desc;
---
+++ 0x0C-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- select
+-- order by desc
+
+select score, name from second_table order by score desc;
+
